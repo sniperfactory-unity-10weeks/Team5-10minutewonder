09c7b2eeaf8d3b092261fce4dd3f518db9ec82a4
doldol123123 <115207259+doldol123123@users.noreply.github.com>
2023-10-12 21:20:56 +0900

    add ui
    item 추가

---
+++ 10-minute wonder/Assets/LeeHyeSoo/Scripts/Item.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Item : ScriptableObject
+{
+    public string itemName;
+    public Sprite itemImage;
+    public string itemMemo;
+
+    
+}
---
+++ 10-minute wonder/Assets/LeeHyeSoo/Scripts/RandomItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomItem : MonoBehaviour
+{
+    public List<Item> items;
+
+
+}
