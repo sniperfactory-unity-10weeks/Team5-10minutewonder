0af4cf5fc5b45b95b13d3a30234c75ad6cc101df
nyyyyyr <127469346+YuuuuuRAE@users.noreply.github.com>
2023-10-12 23:51:35 +0900

    Revert "Merge branch 'main' of https://github.com/sniperfactory-unity-10weeks/Team5-10minutewonder"
    This reverts commit 1ccd3471a1da6cdf1d0188a2e1981bb063ac4977, reversing
    changes made to 4a4dc709512030e7a408514f9067ffaa7ed66677.

--- 10-minute wonder/Assets/Scripts/Drops/Drops.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface Drops
-{
-    // 입력으로 받는 target은 아이템 효과가 적용될 대상
-    void Use(GameObject target);
-}
